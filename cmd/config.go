27ae63cfa92da4b573feece3385dff633cf59b43
Rohit Singh <rkochhar@uwaterloo.ca>
2023-03-24 20:55:21 -0400

    added endpoint generation functions

--- cmd/config.go
+++ cmd/config.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"k8s.io/apimachinery/pkg/util/yaml"
@@ -40,12 +41,50 @@ func (c FlagConfig) getModName() string {
 	return c.ModName
 }
 
+// EndpointDefinition contains information about
+// custom endpoints defined in YAML configuration files
+type EndpointDefinition struct {
+	Path         string `yaml:"path"`
+	Method       string `yaml:"method"`
+	Description  string `yaml:"description"`
+	functionName string
+}
+
+// Returns a templated string describing a handler function
+// for the given endpoint
+func (e *EndpointDefinition) GenerateHandlerFunction() string {
+	// Set the function name to a camelCase alphanumeric representation
+	// of the endpoint path
+
+	e.functionName = fmt.Sprintf("%s%sHandler", capitalizeAfterSlash(e.Path), e.Method)
+
+	templateFunction := `
+	// %s
+	func (a *application) %s(w http.ResponseWriter, r *http.Request) {
+		replyTextContent(w, r, http.StatusOK, "OK")
+	} 
+	`
+	return fmt.Sprintf(templateFunction, e.Description, e.functionName)
+}
+
+// Returns a templated string to append to README.md API Endpoint table
+func (e *EndpointDefinition) GenerateReadmeTableEntry() string {
+	return fmt.Sprintf("| %s | %s | %s |\n", e.Path, e.Method, e.Description)
+}
+
+// Returns a templated line of code attaching handler function to path via router
+func (e *EndpointDefinition) GenerateRouterAttachment() string {
+	// ToDo: Format method into http.MethodXYZ form rather than just XYZ
+	return fmt.Sprintf("router.HandlerFunc(%s, %s, a.%s)", e.Method, e.Path, e.functionName)
+}
+
 // YamlConfig contains app information collected
 // from YAML configuration file
 type YamlConfig struct {
-	AppName   string `yaml:"appName"`
-	Directory string `yaml:"directory"`
-	ModName   string `yaml:"modName"`
+	AppName   string               `yaml:"appName"`
+	Directory string               `yaml:"directory"`
+	ModName   string               `yaml:"modName"`
+	Endpoints []EndpointDefinition `yaml:"endpoints"`
 }
 
 // Returns name of application
@@ -96,7 +135,6 @@ func loadYamlConfig(filename string) (*YamlConfig, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("HERE: %+v", yamlConf)
 	if yamlConf.AppName == "" {
 		return nil, fmt.Errorf("no `appName` field set in YAML file")
 	}
@@ -135,4 +173,26 @@ func loadFlagConfig(cmd *cobra.Command) (*FlagConfig, error) {
 		ModName:   modName,
 		Directory: dir,
 	}, nil
+}
+
+// Returns an alphanumeric camelcase representation of a
+// path-style endpoint (i.e. /v1/healthcheck -> v1Healthcheck)
+func capitalizeAfterSlash(s string) string {
+	var result string
+	capitalize := true
+
+	for _, c := range s {
+		if c == '/' {
+			capitalize = true
+		} else {
+			if capitalize {
+				result += strings.ToUpper(string(c))
+				capitalize = false
+			} else {
+				result += string(c)
+			}
+		}
+	}
+
+	return result
 }
--- test/go.mod
+++
@@ -1,5 +0,0 @@
-module test
-
-go 1.19
-
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
